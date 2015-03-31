4c08144f017309c3961b62f63a19567bdc104665
yurifrl <yurifrl@hotmail.com>
2015-03-30 23:33:14 -0300

    start guide

---
+++ app/routes/application.js
@@ -0,0 +1,6 @@
+export default Ember.Route.extend({
+  setupController: function(controller) {
+    // `controller` is the instance of ApplicationController
+    controller.set('title', "Hello world!");
+  }
+});
---
+++ app/routes/favorites.js
@@ -0,0 +1,5 @@
+import Ember from 'ember';
+
+export default Ember.Router.extend().map(function(){
+  this.route('favorites');
+});
