b8caef87d6620e0c6324c68bd7423e39f856388b
yurifrl <yurifrl@hotmail.com>
2015-04-04 14:05:11 -0300

    ember blog with rails backend start

--- app/routes/application.js
+++
@@ -1,8 +0,0 @@
-/*global Ember */
-
-export default Ember.Route.extend({
-  setupController: function(controller) {
-    // `controller` is the instance of ApplicationController
-    controller.set('title', "Application");
-  }
-});
